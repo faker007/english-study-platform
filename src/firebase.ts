13c295b11038e99752a8b7995f0369a765090974
faker007 <rlaqjadud195@naver.com>
2023-03-15 22:42:55 +0900

    add: some folder structures and navigation, and also firebase

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaR39WZAPFqhxDooT9NR5LWf9GqPNlWQI",
+  authDomain: "english-study-platform.firebaseapp.com",
+  projectId: "english-study-platform",
+  storageBucket: "english-study-platform.appspot.com",
+  messagingSenderId: "698987553808",
+  appId: "1:698987553808:web:334b4ec1ebc59f51581695",
+};
+
+export const app = initializeApp(firebaseConfig);
